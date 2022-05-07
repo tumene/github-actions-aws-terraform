4357f17d143faefb37bdb82c276bb4c2e7e2be03
Tunde Olowojolu <michaelojolu@gmail.com>
2022-05-07 17:53:57 +0100

    modified terraform.tf

--- environments/development/terraform.tf
+++ environments/development/terraform.tf
@@ -1,10 +1,10 @@
 # Require TF version to be same as or greater than 0.12.13
 terraform {
-  required_version = ">=0.12.13"
+  required_version = ">=1.1.15"
   backend "s3" {
-    bucket         = "github-actions-ci"
+    bucket         = "github-tumene-bucket"
     key            = "terraform-development.tfstate"
-    region         = "eu-west-2"
-    dynamodb_table = "github-actions-ci-locks"
+    region         = "us-east-2"
+    dynamodb_table = "github-tumene-ci-locks"
   }
 }
